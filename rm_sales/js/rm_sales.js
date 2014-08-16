c14b1981ed2987d80d5026e64072408066177c48
allvintage <mfrericks@gmail.com>
2014-08-16 20:33:08 +0200

    JS in Module verschoben und neue Funktionen fuer profileset_form

---
+++ rm_sales/js/rm_sales.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Martin on 04.08.14.
+ */
+jQuery(document).ready(function ($) {    
+    
+    $('.priorities select').change(function() {
+        var nidstring = $(this).attr('id');
+        var res = nidstring.split("_");
+        data = new Object;
+        data['nid'] = res[1];
+        data['prio'] = $(this).val();
+
+        callback_url = Drupal.settings.basePath + 'manage/sales/changepriority/' + data['nid'] + '/' + data['prio'];
+            
+        $.ajax({
+            url: callback_url,
+            type: 'POST',
+            data: data
+        });
+    });
+    
+    $('.comments textarea').on('blur', function() {
+        var nidstring = $(this).attr('id');
+        var res = nidstring.split("_");
+        data = new Object;
+        data['nid'] = res[1];
+        data['text'] = $(this).val();
+
+        callback_url = Drupal.settings.basePath + 'manage/sales/updatecomment/' + data['nid'];
+            
+        $.ajax({
+            url: callback_url,
+            type: 'POST',
+            data: data
+        });
+    });
+    
+    $('.nextactions input').on('blur', function() {
+        var nidstring = $(this).attr('id');
+        var res = nidstring.split("_");
+        data = new Object;
+        data['nid'] = res[1];
+        data['text'] = $(this).val();
+        callback_url = Drupal.settings.basePath + 'manage/sales/updatenextaction/' + data['nid'];
+            
+        $.ajax({
+            url: callback_url,
+            type: 'POST',
+            data: data
+        });
+    });
+    
+    $('.salesguys select').change(function() {
+        var nidstring = $(this).attr('id');
+        var res = nidstring.split("_");
+        data = new Object;
+		data['nid'] = res[1];
+		data['uid'] = $(this).val();
+		callback_url = Drupal.settings.basePath + 'manage/sales/assignprofile/' + data['nid'] + '/' + data['uid'];
+            
+		$.ajax({
+			url: callback_url,
+			type: 'POST',
+			data: data,
+		});
+    });
+
+});
